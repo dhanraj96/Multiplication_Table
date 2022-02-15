a4dd0f814afcbbe6eed1935f6c2bf5bbcf903cf8
surajalhat <myrahulpwar@gmail.com>
2022-02-15 19:14:16 +0530

    Multiplication Table

---
+++ Table/src/Table.java
@@ -0,0 +1,17 @@
+import java.util.scanner;
+class Table tab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            scanner scan = new scanner(system.in);
+            int d,h;
+            System.out.println("Enter the number of table");
+            d=scan.nextInt();
+            for(h=1;h<10;h++)
+            {
+            	System.out.println(d+ "x" +h+ "=" +(d*h)):
+            }
+        
+	}
+
+}
